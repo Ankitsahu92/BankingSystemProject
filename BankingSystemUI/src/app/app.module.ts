1bf41dccd62ac2cd8537199ce5b85080df918a6b
Ankit <urankitsahu92@gmail.com>
2022-10-19 08:16:08 +0530

    installed boostrap and ng prime

--- BankingSystemUI/src/app/app.module.ts
+++ BankingSystemUI/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { ChangePasswordComponent } from './auth/change-password/change-password.component';
+
+import { AlertModule } from 'ngx-bootstrap/alert';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,11 @@ import { ChangePasswordComponent } from './auth/change-password/change-password.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+
+    AppRoutingModule,
+
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
