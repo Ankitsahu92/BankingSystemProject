dc148316c4aef0da7f73d62013647da9be84e14e
Ankit <urankitsahu92@gmail.com>
2022-10-19 08:06:44 +0530

    added loging and change password component

--- BankingSystemUI/src/app/app-routing.module.ts
+++ BankingSystemUI/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChangePasswordComponent } from './auth/change-password/change-password.component';
+import { LoginComponent } from './auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  }, {
+    path: 'change-password',
+    component: ChangePasswordComponent
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./core/dashboard/dashboard.module')
+      .then(m => m.DashboardModule)
+    //canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BankingSystemUI/src/app/app.module.ts
+++ BankingSystemUI/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ChangePasswordComponent } from './auth/change-password/change-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ChangePasswordComponent,
   ],
   imports: [
     BrowserModule,
